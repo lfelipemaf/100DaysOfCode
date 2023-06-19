71f197fb85c65d9a498480d75dfc3e9e6dd18566
lfelipemaf <lfelipemaf@icloud.com>
2023-06-19 09:03:20 -0300

    life counter

--- Days 1 - 10/Day 7 - hangman/main.py
+++ Days 1 - 10/Day 7 - hangman/main.py
@@ -3,17 +3,32 @@ import words
 chosen_word = words.chosen_word
 word_length = len(chosen_word)
 
+end_of_game = False
+
 print(f'\nThe word is: {chosen_word}\n')
 print(word_length* "-")
 print("\n")
 word_guess = []
 for _ in range(word_length):
     word_guess += "_"
+live = 6
+print(words.stages[live])
+while not end_of_game:
+    guess = str(input("Guess a word: ")).lower()
 
-guess = str(input("Guess a word: ")).lower()
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        word_guess[position]= letter
+    if chosen_word.count(guess) == 0:
+        live -= 1
+    print(words.stages[live])
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            word_guess[position]= letter
+    display = ''.join(word_guess)
+    print(display)
 
-print(word_guess)
+    if "_" not in display and live > 0:
+        end_of_game = True
+        print("You win.")
+    elif "_"  in display and live == 0:
+        end_of_game = True
+        print("You lose.")
