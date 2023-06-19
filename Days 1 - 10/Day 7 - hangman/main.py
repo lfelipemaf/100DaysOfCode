37af4e872f9036911ed438fa070f241df30bf55e
lfelipemaf <lfelipemaf@icloud.com>
2023-06-19 09:04:34 -0300

    life counter

--- Days 1 - 10/Day 7 - hangman/main.py
+++ Days 1 - 10/Day 7 - hangman/main.py
@@ -1,19 +1,29 @@
 import words
+def ask():
+    letter = str(input("Guess a word: ")).lower()
+    return letter
+def complete_word(letter_chosen,word_guess):
+    letter = letter_chosen
+    for l in chosen_word:
+        if letter == l:
+            word_guess.append(l)
+        else:
+            word_guess.append("_")
+    word_guessed = ''.join(word_guess)
+    return word_guessed
+
 
 chosen_word = words.chosen_word
-word_length = len(chosen_word)
-
-print(f'\nThe word is: {chosen_word}\n')
-print(word_length* "-")
-print("\n")
+print(len(chosen_word) * "-")
 word_guess = []
-for _ in range(word_length):
-    word_guess += "_"
+letter = ask()
 
-guess = str(input("Guess a word: ")).lower()
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        word_guess[position]= letter
+guessed = complete_word(letter, word_guess)
+print(guessed)
+count = 0
+for i in guessed:
+    if i == '_':
+        count = count + 1
+word_guess = guessed
 
-print(word_guess)
+
