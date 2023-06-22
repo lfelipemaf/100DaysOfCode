1f99683427fa15e83a881df2eb6b9ee8c1aea0ca
lfelipemaf <61876877+lfelipemaf@users.noreply.github.com>
2023-06-22 19:09:29 -0300

    improvements on the code

--- Days 1 - 10/Day 10 - Calculator/main.py
+++ Days 1 - 10/Day 10 - Calculator/main.py
@@ -1,30 +1,37 @@
 import art
 import os
 
-def result(f_num,op,s_num):
-    if f_num == '' or op == '' or s_num == '':
-        return "You did not privide enought info."
-    elif op == '+':
-        return float(f_num) + float(s_num)
-    elif op == '-':
-        return float(f_num) - float(s_num)
-    elif op == '*':
-        return float(f_num) * float(s_num)
-    elif op == '/':
-        return float(f_num) / float(s_num)
-    else:
-        return "Wrong operation sign."
-    
+def add(f_num,s_num):
+    return f_num + s_num
+
+def sub(f_num,s_num):
+    return f_num - s_num
+
+def multy(f_num,s_num):
+    return f_num * s_num
+
+def divide(f_num,s_num):
+    return f_num / s_num
+
+ops ={
+    "+": add,
+    "-": sub,
+    "*": multy,
+    "/": divide,
+}
 
 os.system('cls || clear')
 print(art.logo)
 end_calc = False
 
-first_num = input("What's the first number: ")
+first_num = int(input("What's the first number: "))
 while end_calc is not True:
-    operation = input("\n+\n-\n*\n/\nPick an operation: ")
-    second_num = input("What's the next number: ")
-    result_calc = result(first_num,operation,second_num)
+    for symbol in ops:
+        print(symbol)
+    operation = input("\nPick an operation: ")
+    second_num = int(input("What's the next number: "))
+    calculation = ops[operation]
+    result_calc = calculation(first_num,second_num)
     print(f"{first_num} {operation} {second_num} = {result_calc}")
     run = input(f"Type 'y' to continue calculating with {result_calc}, or 'n' to start a new calculation: ")
     if run == 'y':
