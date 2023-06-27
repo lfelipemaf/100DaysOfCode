48d3846ac2ad86a1ee5509506f3785f04c0cd8bb
lfelipemaf <61876877+lfelipemaf@users.noreply.github.com>
2023-06-27 05:41:10 -0300

    working project

--- Days/15_coffeeMachine/controller.py
+++ Days/15_coffeeMachine/controller.py
@@ -1,23 +1,56 @@
 def report():
     """Returns the report of all the available resources"""
-    print(f"Water: {resources['water']}ml\n"
+    print(f"\nWater: {resources['water']}ml\n"
           f"Milk: {resources['milk']}ml\n"
           f"Coffee: {resources['coffee']}g\n"
-          f"Money: ${resources['water']}\n")
+          f"Money: ${resources['money']}\n")
 
-def check_resources(water,milk,coffee):
-    if resources['water'] < water or resources['milk'] < milk or resources['coffee'] < coffee:
-        return False
-    else:
+
+def check_resources(water, milk, coffee):
+    if resources['water'] >= water and resources['milk'] >= milk and resources['coffee'] >= coffee:
         return True
+    else:
+        return False
 
 
+def process_coins(drink):
+    penny = float(input("\nHow Many pennies?")) * 0.01
+    nickels = float(input("How Many nickels?")) * 0.05
+    dimes = float(input("How Many dimes?")) * 0.10
+    quarter = float(input("How Many quarter?")) * 0.25
+    amount_inserted = penny + nickels + dimes + quarter
+    if amount_inserted >= MENU[drink]['cost']:
+        change = amount_inserted - MENU[drink]['cost']
+        print(f"Thank you, you have inserted ${round(amount_inserted,2)} and your ${round(change,2)} is now at the "
+              f"disposal.")
+        return True
+    else:
+        print(f"Sorry, not enough money. You have inserted ${round(amount_inserted,2)} Pick up your coins at the "
+              f"disposal.")
+        return False
+
+
+def make_coffee(command):
+    """Simply make the coffee"""
+    resources_response = check_resources(water=MENU[command]['ingredients']['water'],
+                                         milk=MENU[command]['ingredients']['milk'],
+                                         coffee=MENU[command]['ingredients']['coffee'])
+    if resources_response:
+        coins_response = process_coins(command)
+        if coins_response:
+                resources['water'] -= MENU[command]['ingredients']['water']
+                resources['milk'] -= MENU[command]['ingredients']['milk']
+                resources['coffee'] -= MENU[command]['ingredients']['coffee']
+                resources['money'] += MENU[command]['cost']
+    else:
+        print("Sorry, not enough resources")
 
 
 MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -39,8 +72,6 @@ MENU = {
         "cost": 3.0,
     }
 }
-
-print(MENU["espresso"]['ingredients']['water'])
 
 resources = {
     "water": 300,
--- Days/15_coffeeMachine/main.py
+++ Days/15_coffeeMachine/main.py
@@ -1,36 +1,12 @@
-"""
-### PROGRAM REQUIREMENTS ###
-1. print report
-2. check resources are sufficient?
-3. process coins
-4. check transaction successful?
-5.Make coffee
-"""
 import controller
 
+operation = False
+while not operation:
+    action = input("Hello, what would you like?\nEspresso\nLatte\nCappuccino\n").lower()
 
-
-def make_coffee(command):
-    """Simply make the coffee"""
-    response = controller.check_resources(water=controller.MENU[command]['ingredients']['water'], milk=controller.MENU[command]['ingredients']['milk'],coffee=controller.MENU[command]['ingredients']['coffee'])
-    if response:
-        controller.resources['water'] = controller.resources['water'] - controller.MENU[command]['ingredients']['water']
-        controller.resources['milk'] = controller.resources['milk'] - controller.MENU[command]['ingredients']['milk']
-        controller.resources['coffee'] = controller.resources['coffee'] - controller.MENU[command]['ingredients']['coffee']
-
-    print(controller.resources)
-
-
-
-
-def process_coins():
-    return
-
-
-
-action = input("Hello, what would you like?\nEspresso\nLatte\nCappuccino\n").lower()
-
-if action == 'report':
-    controller.report()
-else:
-    make_coffee(action)
+    if action == 'report':
+        controller.report()
+    elif action == 'exit':
+        operation = True
+    else:
+        controller.make_coffee(action)
