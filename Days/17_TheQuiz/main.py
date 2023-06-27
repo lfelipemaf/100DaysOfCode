4cc529001c9d9f1be287e709e3941e3658897ec9
lfelipemaf <lfelipemaf@icloud.com>
2023-06-27 16:52:16 -0300

    final code

---
+++ Days/17_TheQuiz/main.py
@@ -0,0 +1,21 @@
+import os
+
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+import os
+os.system("cls || clear")
+question_bank = []
+for i in question_data:
+    n_question = Question(i['text'], i['answer'])
+    question_bank.append(n_question)
+
+end_game = False
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+
+    quiz.next_question()
+
+os.system("cls || clear")
+print(f"\nYou`ve completed the quiz\n"
+      f"Your final score was: {quiz.score}/{len(quiz.question_list)}")
