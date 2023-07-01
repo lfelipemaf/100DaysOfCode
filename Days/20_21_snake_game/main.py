63d2752e6750d3d4c86cac9ad7024bb743e4061b
lfelipemaf <lfelipemaf@icloud.com>
2023-07-01 07:03:39 -0300

    code simplified

--- Days/20_21_snake_game/main.py
+++ Days/20_21_snake_game/main.py
@@ -34,15 +34,9 @@ while game_is_on:
         scoreboard.game_over()
         game_is_on = False
 
-
-
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             scoreboard.game_over()
             game_is_on = False
-
-
 
 screen.exitonclick()
