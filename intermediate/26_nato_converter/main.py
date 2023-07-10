fc76bfde2e74934a7af8fbef3a5b1eec7147aed8
lfelipemaf <lfelipemaf@icloud.com>
2023-07-10 05:29:09 -0300

    Handling with errors

--- intermediate/26_nato_converter/main.py
+++ intermediate/26_nato_converter/main.py
@@ -1,12 +1,17 @@
-import pandas
 import pandas as pd
 
 nato = pd.read_csv("nato_phonetic_alphabet.csv")
+
 nato_dict = {row.letter: row.code for (index, row) in nato.iterrows()}
 
+def generate_phonetic():
+    name = input("Tell me your name?:  ").upper()
+    try:
+        nato_name = [nato_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_name)
 
-name = input("Tell me your name?:  ").upper()
-
-letters =[word for word in name]
-nato_name = [nato_dict[letter] for letter in name]
-print(nato_name)
+generate_phonetic()
---
+++ intermediate/30_json/main.py
@@ -0,0 +1,57 @@
+# Handling with errors
+
+# try:
+#     file = open("file.txt")
+#     a_dict = {"key": "value"}
+#     print(a_dict["key"])
+# except FileNotFoundError:
+#     file = open("file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"This {error_message} key does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error that I made up")
+#
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))
+#
+# if height > 3:
+#     raise ValueError("Human height should not be over 3 meters.")
+# bmi = weight / height ** 2
+# print(bmi)
+#
+# # exercise 1 - IndexError Handling
+# fruits = ["Apple", "Pear", "Orange"]
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError:
+#         print("Fruit pie")
+#     else:
+#         print(fruit + " pie")
+#
+#
+# make_pie(4)
+# # exercise 2 - KeyError Handling
+# facebook_posts = [
+#     {'Likes': 21, 'Comments': 2},
+#     {'Likes': 13, 'Comments': 2, 'Shares': 1},
+#     {'Likes': 33, 'Comments': 8, 'Shares': 3},
+#     {'Comments': 4, 'Shares': 2},
+#     {'Comments': 1, 'Shares': 1},
+#     {'Likes': 19, 'Comments': 3}
+# ]
+#
+# total_likes = 0
+#
+# for post in facebook_posts:
+#     try:
+#         total_likes += post['Likes']
+#     except KeyError:
+#         pass
+#
+#
+# print(total_likes)
