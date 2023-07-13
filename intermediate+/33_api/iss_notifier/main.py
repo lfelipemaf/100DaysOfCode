58163220a346df99c224cf6148beb445aca2f862
lfelipemaf <lfelipemaf@icloud.com>
2023-07-13 07:41:26 -0300

    day 34

---
+++ intermediate+/33_api/iss_notifier/main.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime as dt
+import smtplib
+import creds
+import time
+
+MY_LAT = -20.316839
+MY_LONG = -40.309921
+my_email = creds.cred["user"]
+password = creds.cred["password"]
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get(url=f"https://api.sunrise-sunset.org/json", params=parameters, verify=False)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = dt.utcnow().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=my_email,
+                                msg=f"Subject:ISS NOTIFICATION\n\nLook up now, you can spot ISS.")
---
+++ intermediate+/34_quiz_app/ui.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterFace:
+
+    def __init__(self,quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quiz")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_lbl = Label(text="Score:x",fg="white", bg=THEME_COLOR)
+        self.score_lbl.grid(column=1, row=0)
+
+        self.canvas = Canvas(height=250, width=300,bg="white", highlightthickness=0)
+        self.question = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="question",
+            fill="black",
+            font=("Arial", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50, padx=50)
+
+        true = PhotoImage(file="images/true.png")
+        self.true_btn = Button(image=true, highlightthickness=0, highlightbackground=THEME_COLOR,
+                               highlightcolor=THEME_COLOR, command=self.true_answer)
+        self.true_btn.grid(column=0, row=2)
+
+        false = PhotoImage(file="images/false.png")
+        self.false_btn = Button(image=false, highlightthickness=0, highlightbackground=THEME_COLOR,
+                                highlightcolor=THEME_COLOR, command=self.false_answer)
+        self.false_btn.grid(column=1, row=2)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_lbl.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question, text=f"You've completed the quiz\n"
+                                                       f"Your final score was:"
+                                                       f" {self.quiz.score}/{self.quiz.question_number}")
+            self.score_lbl.config(text="")
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
+
+
+
+    def true_answer(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_answer(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
