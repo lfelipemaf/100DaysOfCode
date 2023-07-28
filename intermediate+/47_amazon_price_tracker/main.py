acdc1bf4a3a9afd62ab4ec5552c1c8fb8da6a68d
lfelipemaf <lfelipemaf@icloud.com>
2023-07-28 17:41:22 -0300

    final project

--- intermediate+/47_amazon_price_tracker/main.py
+++ intermediate+/47_amazon_price_tracker/main.py
@@ -2,18 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-URL = "https://www.amazon.com.br/dp/B0949DC5H6/?coliid=I3QQ0I3WFEI18W&colid=F47WVX7JHT6C&psc=1&ref_=list_c_wl_gv_ov_lig_pi_dp"
+URL = "https://www.amazon.com.br/dp/140954947X/?coliid=IDEY054C1IRHK&colid=F47WVX7JHT6C&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it"
 headers = {
     "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     "Accept-Language": "pt-BR,pt;q=0.9,en;q=0.8"
 }
-target_price = 62.00
+target_price = 77
 
 response = requests.get(url=URL, headers=headers, verify=False)
 
 soup = BeautifulSoup(response.content, "lxml")
 price = float(soup.find(class_="a-offscreen").getText().split("R$")[1].replace(",", "."))
-product_title = soup.find(id="productTitle").getText()
+product_title = soup.find(id="productTitle").getText().strip()
 if price <= target_price:
     print(f"PRICE ALERT\n\n"
           f"The {product_title} you are looking for to buy is now R${str(price).replace('.',',')}\n\n"
